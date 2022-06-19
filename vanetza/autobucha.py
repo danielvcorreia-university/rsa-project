e0326114d3b949125a06af10f1fb6961c2488950
Joaquim Ramos <joaquimramos@ua.pt>
2022-06-20 00:11:47 +0100

    Bus now warns when its stopped
    sends a denm message for each second it is stopped

--- vanetza/autobucha.py
+++ vanetza/autobucha.py
@@ -2,7 +2,9 @@ import time
 import json
 import paho.mqtt.client as mqtt
 import csv
-import threading 
+import threading
+import cv2
+import numpy as np 
 
 #Signal to know when the bus is stopped
 stoped = 0
@@ -40,8 +42,14 @@ with open('bus_data.csv') as file_obj:
         #Sending Cams
         f = open("examples/in_cam.json")
 
+        f1 = open("examples/in_denm.json")
+
+        f2 = open("examples/in_cpm.json")
+
         #Building the message
         message = json.load(f)
+
+        Dmessage = json.load(f1)
 
         message["stationType"] = 6
 
@@ -61,6 +69,26 @@ with open('bus_data.csv') as file_obj:
         if (float(row[13])<0.20):
 
             stoped = 1
+
+            Dmessage["management"]["stationType"] = 6
+
+            Dmessage["management"]["validityDuration"] = 1
+
+            Dmessage["management"]["actionID"]["originatingStationID"] = 1
+
+            Dmessage["management"]["eventPosition"]["latitude"] = float(row[1])
+
+            Dmessage["management"]["eventPosition"]["longitude"] = float(row[2])
+
+            Dmessage["management"]["eventPosition"]["altitude"]["altitudeValue"] = float(row[9])
+            
+            Dmessage["situation"]["eventType"]["causeCode"] = 94
+
+            Dmessage["situation"]["eventType"]["subCauseCode"] = 94
+
+            print(Dmessage)
+
+            client.publish("vanetza/in/denm", json.dumps(Dmessage))
 
         else: 
 
