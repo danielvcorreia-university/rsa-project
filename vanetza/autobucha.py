07ac14b976d9b5b8718ff49282e9bf35ed45003c
Joaquim Ramos <joaquimramos@ua.pt>
2022-06-19 22:22:02 +0100

    preliminary

--- vanetza/autobucha.py
+++ vanetza/autobucha.py
@@ -2,10 +2,26 @@ import time
 import json
 import paho.mqtt.client as mqtt
 import csv
+import threading 
+
+#Signal to know when the bus is stopped
+stoped = 0
+
+#function to process videos to detect persons and cars
+def video_process():
+    
+    while (stoped==1):
+        
+        print("\n hello")
+
+        time.sleep(1)
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
+
+t1 = threading.Thread(target=video_process)
+t1.start()
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -24,6 +40,7 @@ with open('bus_data.csv') as file_obj:
         #Sending Cams
         f = open("examples/in_cam.json")
 
+        #Building the message
         message = json.load(f)
 
         message["stationType"] = 6
@@ -40,8 +57,20 @@ with open('bus_data.csv') as file_obj:
 
         message["speed"] = float(row[13])
 
+        #Detecting when the bus has stopped
+        if (float(row[13])<0.20):
+
+            stoped = 1
+
+        else: 
+
+            stoped = 0    
+
         print(message)
 
+        print(stoped)
+
+        #Sending the message
         client.publish("vanetza/in/cam", json.dumps(message))
 
         time.sleep(1)
