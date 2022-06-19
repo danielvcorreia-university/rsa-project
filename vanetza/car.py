8691fe8d7c8fe2e95193986c2e88358b49267ce6
Joaquim Ramos <joaquimramos@ua.pt>
2022-06-19 22:33:12 +0100

    car with data publishing

--- vanetza/car.py
+++ vanetza/car.py
@@ -1,4 +1,7 @@
+import time
+import json
 import paho.mqtt.client as mqtt
+import csv
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -16,6 +19,41 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect("192.168.98.20", 1884, 60)
+
+with open('car_data.csv') as file_obj:
+
+    # Create reader object by passing the file 
+    # object to reader method
+    reader_obj = csv.reader(file_obj)
+
+    for row in reader_obj:
+
+        #Sending Cams
+        f = open("examples/in_cam.json")
+
+        #Building the message
+        message = json.load(f)
+
+        message["stationType"] = 5
+
+        message["stationID"] = 2
+
+        message["altitude"] = float(row[9])
+
+        message["heading"] = float(row[14])
+
+        message["latitude"] = float(row[1])
+
+        message["longitude"] = float(row[2])
+
+        message["speed"] = float(row[13])
+
+        print(message)
+
+        #Sending the message
+        client.publish("vanetza/in/cam", json.dumps(message))
+
+        time.sleep(1)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
